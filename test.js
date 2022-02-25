1f05a8596ca66399309440865407aa31cab32a62
kangho <kangsick98@gmail.com>
2022-02-25 10:49:09 +0900

    Git: 1부터 50의 자연수 중 짝수를 구하는 함수

---
+++ test.js
@@ -0,0 +1,12 @@
+const returnEven = () => {
+  const arr = [];
+  for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+      arr.push(i);
+    }
+  }
+
+  return arr;
+};
+
+console.log(returnEven());
